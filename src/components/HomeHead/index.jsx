a1a1bd83eeccd6d9b2fc066cf50f005c79b8223d
550775082@qq.com <550775082@qq.com>
2018-10-06 20:37:04 +0800

    头像

--- src/components/HomeHead/index.jsx
+++ src/components/HomeHead/index.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import FontAwesome from 'react-fontawesome';
 import {City,Login} from '../../constants/url';
 import SearchInput from '../SearchInput';
+import Img from '../Img';
 require('./style.sass');
 class HomeHead extends Component {
     constructor(props){
@@ -22,7 +23,7 @@ class HomeHead extends Component {
                 <SearchInput/>
                 <div className="user-info">
                     {this.props.userInfo.username 
-                        ? ""
+                        ? <Link to={Login}><Img src="headimage.jpg"/></Link>
                         : <Link to={Login}><FontAwesome name='user'/></Link>
                     }
                 </div>
